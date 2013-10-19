d1844ae6f711ab50c54c4c12f738b7ee2ad13144
Akshay <akshay30nov@gmail.com>
2013-10-20 00:30:59 +0530

    Initialize the drawing class

---
+++ src/in/co/sdslabs/sketchbook/DrawingView.java
@@ -0,0 +1,90 @@
+package in.co.sdslabs.sketchbook;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingView extends View{
+	
+	
+	//Initialise required variables
+	private Path drawPath;
+	private Paint drawPaint , canvasPaint;
+	private int color = 0xFF660000;
+	private Canvas canvas;
+	private Bitmap bmp;
+	
+
+	public DrawingView(Context context , AttributeSet attr) {
+		super(context , attr);
+		setupDrawing();
+		
+	}
+	
+	private void setupDrawing()
+	{
+		drawPath = new Path();
+		drawPaint = new Paint();
+		
+		drawPaint.setColor(color);
+		
+		drawPaint.setAntiAlias(true);
+		drawPaint.setStrokeWidth(20);
+		drawPaint.setStyle(Paint.Style.STROKE);
+		drawPaint.setStrokeJoin(Paint.Join.ROUND);
+		drawPaint.setStrokeCap(Paint.Cap.ROUND);
+	
+		canvasPaint = new Paint(Paint.DITHER_FLAG);
+	}
+	
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		
+		bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+		canvas = new Canvas(bmp);
+		
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+	//draw view
+		canvas.drawBitmap(bmp, 0, 0, canvasPaint);
+		canvas.drawPath(drawPath, drawPaint);
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		//detect user touch
+		
+		float touchX = event.getX();
+		float touchY = event.getY();
+		
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+		    drawPath.moveTo(touchX, touchY);
+		    break;
+		case MotionEvent.ACTION_MOVE:
+		    drawPath.lineTo(touchX, touchY);
+		    break;
+		case MotionEvent.ACTION_UP:
+		    canvas.drawPath(drawPath, drawPaint);
+		    drawPath.reset();
+		    break;
+		default:
+		    return false;
+		}
+		
+		invalidate();
+		return true;
+		
+	}
+	
+	
+}
