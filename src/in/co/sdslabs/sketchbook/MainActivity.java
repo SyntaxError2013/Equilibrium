f023e9b6eda08a7aa75ec76b1b4b60d58f85c72d
Akshay <akshay30nov@gmail.com>
2013-10-20 15:06:25 +0530

    Implement color selector

--- src/in/co/sdslabs/sketchbook/MainActivity.java
+++ src/in/co/sdslabs/sketchbook/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	
 	private DrawingView drawView;
 	
-	private ImageButton newFile , brush , eraser , saveFile ;
+	private ImageButton newFile , brush , eraser , saveFile , colorChoose;
 	private float smallBrush , mediumBrush , largeBrush;
 
 	@Override
@@ -33,7 +33,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		saveFile = (ImageButton) findViewById(R.id.saveFile);
 		brush = (ImageButton) findViewById(R.id.brush);
 		eraser = (ImageButton) findViewById(R.id.eraser);
-		
+		colorChoose = (ImageButton) findViewById(R.id.colorchoose);
 		smallBrush = getResources().getInteger(R.integer.small_size);
 		mediumBrush = getResources().getInteger(R.integer.medium_size);
 		largeBrush = getResources().getInteger(R.integer.large_size);
@@ -42,6 +42,7 @@ public class MainActivity extends Activity implements OnClickListener{
 		saveFile.setOnClickListener(this);
 		brush.setOnClickListener(this);
 		eraser.setOnClickListener(this);
+		colorChoose.setOnClickListener(this);
 		
 		drawView.setBrushSize(mediumBrush);
 	}
@@ -99,7 +100,6 @@ public class MainActivity extends Activity implements OnClickListener{
 			    public void onClick(DialogInterface dialog, int which){
 			    	drawView.setDrawingCacheEnabled(true);
 			    	
-			    	// While doing new input the name of the string
 			    	// Custom Location to be done 
 			    	String imgSaved = MediaStore.Images.Media.insertImage(
 			    		    getContentResolver(), drawView.getDrawingCache(),
@@ -207,6 +207,89 @@ public class MainActivity extends Activity implements OnClickListener{
 			brushDialog.show();
 			
 			break;
+		}
+		case R.id.colorchoose :
+		{
+			final Dialog colorDialog = new Dialog(this);
+			colorDialog.setTitle("Choose color : ");
+			colorDialog.setContentView(R.layout.color_chooser);
+			
+			
+			
+			ImageButton redBtn = (ImageButton)colorDialog.findViewById(R.id.color_red);
+			redBtn.setOnClickListener(new OnClickListener(){
+			    @Override
+			    public void onClick(View v) {
+			        
+			    	String color = v.getTag().toString();
+			    	drawView.setColor(color);
+			    	drawView.setErase(false);
+			        colorDialog.dismiss();
+			    }
+			});
+			
+			ImageButton  greenBtn = (ImageButton)colorDialog.findViewById(R.id.color_green);
+			greenBtn.setOnClickListener(new OnClickListener(){
+			    @Override
+			    public void onClick(View v) {
+			        
+			    	String color = v.getTag().toString();
+			    	drawView.setColor(color);
+			    	drawView.setErase(false);
+			        colorDialog.dismiss();
+			    }
+			});
+			
+			ImageButton pinkBtn = (ImageButton)colorDialog.findViewById(R.id.color_pink);
+			pinkBtn.setOnClickListener(new OnClickListener(){
+			    @Override
+			    public void onClick(View v) {
+			        
+			    	String color = v.getTag().toString();
+			    	drawView.setColor(color);
+			    	drawView.setErase(false);
+			        colorDialog.dismiss();
+			    }
+			});
+			
+			ImageButton blueBtn = (ImageButton)colorDialog.findViewById(R.id.color_blue);
+			blueBtn.setOnClickListener(new OnClickListener(){
+			    @Override
+			    public void onClick(View v) {
+			        
+			    	String color = v.getTag().toString();
+			    	drawView.setColor(color);
+			    	drawView.setErase(false);
+			        colorDialog.dismiss();
+			    }
+			});
+			
+			ImageButton brownBtn = (ImageButton)colorDialog.findViewById(R.id.color_brown);
+			brownBtn.setOnClickListener(new OnClickListener(){
+			    @Override
+			    public void onClick(View v) {
+			        
+			    	String color = v.getTag().toString();
+			    	drawView.setColor(color);
+			    	drawView.setErase(false);
+			        colorDialog.dismiss();
+			    }
+			});
+			
+			ImageButton blackBtn = (ImageButton)colorDialog.findViewById(R.id.color_black);
+			blackBtn.setOnClickListener(new OnClickListener(){
+			    @Override
+			    public void onClick(View v) {
+			        
+			    	String color = v.getTag().toString();
+			    	drawView.setColor(color);
+			    	drawView.setErase(false);
+			        colorDialog.dismiss();
+			    }
+			});
+			
+			
+			colorDialog.show();
 		}
 		
 		}
